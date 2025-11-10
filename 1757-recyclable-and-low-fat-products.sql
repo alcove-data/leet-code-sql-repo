24662ada8498159c61aad64c8afa4e5c81204432
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-10 07:39:23 -0500

    Implement query for low-fat and recyclable products
    Add SQL query to select low-fat and recyclable products.

--- 1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products.sql
@@ -1,5 +1,28 @@
 #1757. Recyclable and Low Fat Products
 
+#Input:
+#Products table:
+#+-------------+----------+------------+
+#| product_id  | low_fats | recyclable |
+#+-------------+----------+------------+
+#| 0           | Y        | N          |
+#| 1           | Y        | Y          |
+#| 2           | N        | Y          |
+#| 3           | Y        | Y          |
+#| 4           | N        | N          |
+#+-------------+----------+------------+
+
+    
+# Selects ID of products that are both low-fat and recyclable, then returns only these IDs
 SELECT product_id
     FROM Products
     WHERE low_fats = 'Y' AND recyclable = 'Y';
+
+
+#Output: 
+#+-------------+
+#| product_id  |
+#+-------------+
+#| 1           |
+#| 3           |
+#+-------------+
