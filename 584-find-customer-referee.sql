12b1aadcef7e795b6ca18af8a88c4327334d501d
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-10 07:45:05 -0500

    Create SQL script to find customer referees
    Add SQL query to find customers not referred by referee_id 2

---
+++ 584-find-customer-referee.sql
@@ -0,0 +1,40 @@
+#584. Find Customer Referee
+
+#Table: Customer
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+
+Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+
+#Selects name of customer not referred by id=2 or not referred by any customer
+  SELECT name
+    FROM Customer
+    WHERE referee_id != 2 OR referee_id IS NULL;
+
+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
