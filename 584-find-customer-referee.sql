e0d85ed076224409251522c179c53691584f8be2
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-10 13:53:27 -0500

    Update comments for clarity in SQL script

--- 584-find-customer-referee.sql
+++ 584-find-customer-referee.sql
@@ -10,30 +10,30 @@
 #+-------------+---------+
 
 #Input: 
-#+----+------+------------+
-#| id | name | referee_id |
-#+----+------+------------+
-#| 1  | Will | null       |
-#| 2  | Jane | null       |
-#| 3  | Alex | 2          |
-#| 4  | Bill | null       |
-#| 5  | Zack | 1          |
-#| 6  | Mark | 2          |
-#+----+------+------------+
+#  +----+------+------------+
+#  | id | name | referee_id |
+#  +----+------+------------+
+#  | 1  | Will | null       |
+#  | 2  | Jane | null       |
+#  | 3  | Alex | 2          |
+#  | 4  | Bill | null       |
+#  | 5  | Zack | 1          |
+#  | 6  | Mark | 2          |
+#  +----+------+------------+
 
   
-#Selects name of customer not referred by id=2 or not referred by any customer
+#Selects the name of customers not referred by id=2 or by any customer
   SELECT name
     FROM Customer
     WHERE referee_id != 2 OR referee_id IS NULL;
 
 
 #Output: 
-#+------+
-#| name |
-#+------+
-#| Will |
-#| Jane |
-#| Bill |
-#| Zack |
-#+------+
+#  +------+
+#  | name |
+#  +------+
+#  | Will |
+#  | Jane |
+#  | Bill |
+#  | Zack |
+#  +------+
