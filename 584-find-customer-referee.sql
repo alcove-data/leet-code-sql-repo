883e88167054caeec83b4a91373140ae9f419184
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-10 07:46:12 -0500

    Remove redundant comments from SQL file

--- 584-find-customer-referee.sql
+++ 584-find-customer-referee.sql
@@ -1,40 +1,39 @@
 #584. Find Customer Referee
 
 #Table: Customer
+#+-------------+---------+
+#| Column Name | Type    |
+#+-------------+---------+
+#| id          | int     |
+#| name        | varchar |
+#| referee_id  | int     |
+#+-------------+---------+
 
-+-------------+---------+
-| Column Name | Type    |
-+-------------+---------+
-| id          | int     |
-| name        | varchar |
-| referee_id  | int     |
-+-------------+---------+
+#Input: 
+#+----+------+------------+
+#| id | name | referee_id |
+#+----+------+------------+
+#| 1  | Will | null       |
+#| 2  | Jane | null       |
+#| 3  | Alex | 2          |
+#| 4  | Bill | null       |
+#| 5  | Zack | 1          |
+#| 6  | Mark | 2          |
+#+----+------+------------+
 
-Input: 
-Customer table:
-+----+------+------------+
-| id | name | referee_id |
-+----+------+------------+
-| 1  | Will | null       |
-| 2  | Jane | null       |
-| 3  | Alex | 2          |
-| 4  | Bill | null       |
-| 5  | Zack | 1          |
-| 6  | Mark | 2          |
-+----+------+------------+
-
+  
 #Selects name of customer not referred by id=2 or not referred by any customer
   SELECT name
     FROM Customer
     WHERE referee_id != 2 OR referee_id IS NULL;
 
 
-Output: 
-+------+
-| name |
-+------+
-| Will |
-| Jane |
-| Bill |
-| Zack |
-+------+
+#Output: 
+#+------+
+#| name |
+#+------+
+#| Will |
+#| Jane |
+#| Bill |
+#| Zack |
+#+------+
