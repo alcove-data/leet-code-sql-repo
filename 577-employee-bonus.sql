681c975724a7d02f9889bc4405cfa80c712fd64f
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-11 16:47:35 -0500

    Refactor SQL comments in employee bonus script
    Updated SQL comments and formatting for clarity.

---
+++ 577-employee-bonus.sql
@@ -0,0 +1,55 @@
+
+#577. Employee Bonus
+
+#Table: Employee
+#+-------------+---------+
+#| Column Name | Type    |
+#+-------------+---------+
+#| empId       | int     |
+#| name        | varchar |
+#| supervisor  | int     |
+#| salary      | int     |
+#+-------------+---------+
+
+#Table: Bonus
+#+-------------+------+
+#| Column Name | Type |
+#+-------------+------+
+#| empId       | int  |
+#| bonus       | int  |
+#+-------------+------+
+
+#Input: 
+#  Employee table:
+#  +-------+--------+------------+--------+
+#  | empId | name   | supervisor | salary |
+#  +-------+--------+------------+--------+
+#  | 3     | Brad   | null       | 4000   |
+#  | 1     | John   | 3          | 1000   |
+#  | 2     | Dan    | 3          | 2000   |
+#  | 4     | Thomas | 3          | 4000   |
+#  +-------+--------+------------+--------+
+#Bonus table:
+#  +-------+-------+
+#  | empId | bonus |
+#  +-------+-------+
+#  | 2     | 500   |
+#  | 4     | 2000  |
+#  +-------+-------+
+
+#Joins the Employee table with the Bonus table, then returns the name and bonus when less than 1000 or when null
+SELECT Employee.name, Bonus.bonus
+    FROM Employee
+    LEFT JOIN Bonus ON Employee.empID = Bonus.empID
+    WHERE Bonus.bonus < 1000 OR Bonus.bonus IS NULL
+    GROUP BY Employee.name
+
+  
+#Output: 
+#  +------+-------+
+#  | name | bonus |
+#  +------+-------+
+#  | Brad | null  |
+#  | John | null  |
+#  | Dan  | 500   |
+#  +------+-------+
