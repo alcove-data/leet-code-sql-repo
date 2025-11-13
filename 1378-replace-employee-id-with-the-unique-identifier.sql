189a895525fc07cfeff10841c7f6eb9e33ad5ffe
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-13 13:27:01 -0500

    Update 1378-replace-employee-id-with-the-unique-identifier.sql

--- 1378-replace-employee-id-with-the-unique-identifier.sql
+++ 1378-replace-employee-id-with-the-unique-identifier.sql
@@ -1,57 +1,68 @@
-#1378. Replace Employee ID With The Unique Identifier
+/*
+1378. Replace Employee ID With The Unique Identifier
 
-#Table: Employees
-#+---------------+---------+
-#| Column Name   | Type    |
-#+---------------+---------+
-#| id            | int     |
-#| name          | varchar |
-#+---------------+---------+
+Write a solution to show the unique ID of each user.
+If a user does not have a unique ID replace just show null.
+Return the result table in any order.
+The result format is in the following example.
 
-#Table: EmployeeUNI
-#+---------------+---------+
-#| Column Name   | Type    |
-#+---------------+---------+
-#| id            | int     |
-#| unique_id     | int     |
-#+---------------+---------+
 
-#Input: 
-#  Employees:
-#  +----+----------+
-#  | id | name     |
-#  +----+----------+
-#  | 1  | Alice    |
-#  | 7  | Bob      |
-#  | 11 | Meir     |
-#  | 90 | Winston  |
-#  | 3  | Jonathan |
-#  +----+----------+
+Table: Employees
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
 
-#  EmployeeUNI:
-#  +----+-----------+
-#  | id | unique_id |
-#  +----+-----------+
-#  | 3  | 1         |
-#  | 11 | 2         |
-#  | 90 | 3         |
-#  +----+-----------+
+Table: EmployeeUNI
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
 
-  
-#Joins Employees table with EmployeeUNI table, then returns the name of each employee and their unique ID; otherwise null
-SELECT EmployeeUNI.unique_id, Employees.name
+Input: 
+  Employees:
+  +----+----------+
+  | id | name     |
+  +----+----------+
+  | 1  | Alice    |
+  | 7  | Bob      |
+  | 11 | Meir     |
+  | 90 | Winston  |
+  | 3  | Jonathan |
+  +----+----------+
+
+  EmployeeUNI:
+  +----+-----------+
+  | id | unique_id |
+  +----+-----------+
+  | 3  | 1         |
+  | 11 | 2         |
+  | 90 | 3         |
+  +----+-----------+
+*/
+
+
+SELECT
+  EmployeeUNI.unique_id, -- selects the unique id from from EmployeeUNI table
+  Employees.name -- selects the employee's name from Employees table
 FROM EmployeeUNI
-RIGHT JOIN Employees
-ON EmployeeUNI.id = Employees.id
+RIGHT JOIN Employees -- considers all employees with or without a matching unique_id
+ON EmployeeUNI.id = Employees.id -- joins on id to match employees with their unique ids
 
-  
-#Output: 
-#  +-----------+----------+
-#  | unique_id | name     |
-#  +-----------+----------+
-#  | null      | Alice    |
-#  | null      | Bob      |
-#  | 2         | Meir     |
-#  | 3         | Winston  |
-#  | 1         | Jonathan |
-#  +-----------+----------+
+
+/*
+Output: 
+  +-----------+----------+
+  | unique_id | name     |
+  +-----------+----------+
+  | null      | Alice    |
+  | null      | Bob      |
+  | 2         | Meir     |
+  | 3         | Winston  |
+  | 1         | Jonathan |
+  +-----------+----------+
+*/
