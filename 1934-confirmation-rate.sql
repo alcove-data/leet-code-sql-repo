37472b7e156be8ffe9695f52a46c0ef6d8e3995c
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-13 13:03:18 -0500

    Update comments and formatting in SQL file

--- 1934-confirmation-rate.sql
+++ 1934-confirmation-rate.sql
@@ -1,70 +1,75 @@
-#1934. Confirmation Rate
+/*
+1934. Confirmation Rate
 
-#The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages.
-#The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
-#Write a solution to find the confirmation rate of each user.
-#Return the result table in any order.
-#The result format is in the following example.
+The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages.
+The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+Write a solution to find the confirmation rate of each user.
+Return the result table in any order.
+The result format is in the following example.
 
-#Table: Signups
-#+----------------+----------+
-#| Column Name    | Type     |
-#+----------------+----------+
-#| user_id        | int      |
-#| time_stamp     | datetime |
-#+----------------+----------+
+Table: Signups
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
++----------------+----------+
 
 Table: Confirmations
-#+----------------+----------+
-#| Column Name    | Type     |
-#+----------------+----------+
-#| user_id        | int      |
-#| time_stamp     | datetime |
-#| action         | ENUM     |
-#+----------------+----------+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
+| action         | ENUM     |
++----------------+----------+
 
---Input: 
-#  Signups:
-#  +---------+---------------------+
-#  | user_id | time_stamp          |
-#  +---------+---------------------+
-#  | 3       | 2020-03-21 10:16:13 |
-#  | 7       | 2020-01-04 13:57:59 |
-#  | 2       | 2020-07-29 23:09:44 |
-#  | 6       | 2020-12-09 10:39:37 |
-#  +---------+---------------------+
+Input: 
+  Signups:
+  +---------+---------------------+
+  | user_id | time_stamp          |
+  +---------+---------------------+
+  | 3       | 2020-03-21 10:16:13 |
+  | 7       | 2020-01-04 13:57:59 |
+  | 2       | 2020-07-29 23:09:44 |
+  | 6       | 2020-12-09 10:39:37 |
+  +---------+---------------------+
 
-#  Confirmations:
-#  +---------+---------------------+-----------+
-#  | user_id | time_stamp          | action    |
-#  +---------+---------------------+-----------+
-#  | 3       | 2021-01-06 03:30:46 | timeout   |
-#  | 3       | 2021-07-14 14:00:00 | timeout   |
-#  | 7       | 2021-06-12 11:57:29 | confirmed |
-#  | 7       | 2021-06-13 12:58:28 | confirmed |
-#  | 7       | 2021-06-14 13:59:27 | confirmed |
-#  | 2       | 2021-01-22 00:00:00 | confirmed |
-#  | 2       | 2021-02-28 23:59:59 | timeout   |
-#  +---------+---------------------+-----------+
-
+  Confirmations:
+  +---------+---------------------+-----------+
+  | user_id | time_stamp          | action    |
+  +---------+---------------------+-----------+
+  | 3       | 2021-01-06 03:30:46 | timeout   |
+  | 3       | 2021-07-14 14:00:00 | timeout   |
+  | 7       | 2021-06-12 11:57:29 | confirmed |
+  | 7       | 2021-06-13 12:58:28 | confirmed |
+  | 7       | 2021-06-14 13:59:27 | confirmed |
+  | 2       | 2021-01-22 00:00:00 | confirmed |
+  | 2       | 2021-02-28 23:59:59 | timeout   |
+  +---------+---------------------+-----------+
+*/
 
 SELECT
-    Signups.user_id,
+    Signups.user_id, -- selects each user_id
     ROUND(
         CASE
-            WHEN COUNT(Confirmations.user_id) = 0 THEN 0
-            ELSE SUM(CASE WHEN Confirmations.action = 'confirmed' THEN 1 ELSE 0 END) / COUNT(Confirmations.user_id) END, 2) AS confirmation_rate
-FROM Signups
-LEFT JOIN Confirmations ON Signups.user_id = Confirmations.user_id
-GROUP By Signups.user_id
+            WHEN COUNT(Confirmations.user_id) = 0 THEN 0 -- defines having no confirmation records as rate = 0
+            ELSE
+                SUM(CASE WHEN Confirmations.action = 'confirmed' THEN 1 ELSE 0 END) -- counts confirmed actions
+                / COUNT(Confirmations.user_id) END, 2) AS confirmation_rate -- divide by total confirmation requests
+FROM Signups -- table containing all users
+LEFT JOIN Confirmations ON Signups.user_id = Confirmations.user_id -- join confirmation attempts and match users to their confirmation records
+GROUP By Signups.user_id -- aggregate by user
 
 
-#  Output: 
-#  +---------+-------------------+
-#  | user_id | confirmation_rate |
-#  +---------+-------------------+
-#  | 6       | 0.00              |
-#  | 3       | 0.00              |
-#  | 7       | 1.00              |
-#  | 2       | 0.50              |
-#  +---------+-------------------+
+/*
+  Output: 
+  +---------+-------------------+
+  | user_id | confirmation_rate |
+  +---------+-------------------+
+  | 6       | 0.00              |
+  | 3       | 0.00              |
+  | 7       | 1.00              |
+  | 2       | 0.50              |
+  +---------+-------------------+
+*/
