5267b2fa4dea8e7914e5854c595f2002fd499358
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-13 13:09:38 -0500

    Update 1148-article-views-1.sql

--- 1148-article-views-1.sql
+++ 1148-article-views-1.sql
@@ -1,40 +1,48 @@
-#1148. Article Views I
+/*
+1148. Article Views I
 
-#Table: Views
-#+---------------+---------+
-#| Column Name   | Type    |
-#+---------------+---------+
-#| article_id    | int     |
-#| author_id     | int     |
-#| viewer_id     | int     |
-#| view_date     | date    |
-#+---------------+---------+
-
-#Input: 
-#  +------------+-----------+-----------+------------+
-#  | article_id | author_id | viewer_id | view_date  |
-#  +------------+-----------+-----------+------------+
-#  | 1          | 3         | 5         | 2019-08-01 |
-#  | 1          | 3         | 6         | 2019-08-02 |
-#  | 2          | 7         | 7         | 2019-08-01 |
-#  | 2          | 7         | 6         | 2019-08-02 |
-#  | 4          | 7         | 1         | 2019-07-22 |
-#  | 3          | 4         | 4         | 2019-07-21 |
-#  | 3          | 4         | 4         | 2019-07-21 |
-#  +------------+-----------+-----------+------------+
+Write a solution to find all the authors that viewed at least one of their own articles.
+Return the result table sorted by id in ascending order.
+The result format is in the following example.
 
 
-#selects the ID of all authors that viewed their own articles at least once
-SELECT DISTINCT author_id AS id
-    FROM Views
-    WHERE author_id = viewer_id
-    ORDER BY author_id ASC;
+Table: Views
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+
+Input: 
+  +------------+-----------+-----------+------------+
+  | article_id | author_id | viewer_id | view_date  |
+  +------------+-----------+-----------+------------+
+  | 1          | 3         | 5         | 2019-08-01 |
+  | 1          | 3         | 6         | 2019-08-02 |
+  | 2          | 7         | 7         | 2019-08-01 |
+  | 2          | 7         | 6         | 2019-08-02 |
+  | 4          | 7         | 1         | 2019-07-22 |
+  | 3          | 4         | 4         | 2019-07-21 |
+  | 3          | 4         | 4         | 2019-07-21 |
+  +------------+-----------+-----------+------------+
+*/
 
 
-#Output: 
-#  +------+
-#  | id   |
-#  +------+
-#  | 4    |
-#  | 7    |
-#  +------+
+SELECT DISTINCT author_id AS id -- selects unique author ids and renames the column to 'id'
+    FROM Views -- table containing view records
+    WHERE author_id = viewer_id -- filters rows where author viewed their own article
+    ORDER BY author_id ASC; -- sort results by author id in ascending order
+
+
+/*
+Output: 
+  +------+
+  | id   |
+  +------+
+  | 4    |
+  | 7    |
+  +------+
+*/
