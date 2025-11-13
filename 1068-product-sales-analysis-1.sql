234f6c426d18c2f448ce82cb222c77e89834f52f
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-13 13:43:59 -0500

    Update and rename 1608-product-sales-analysis-1.sql to 1068-product-sales-analysis-1.sql

--- 1068-product-sales-analysis-1.sql
+++ 1068-product-sales-analysis-1.sql
@@ -1,6 +1,11 @@
 /*
 1068. Product Sales Analysis I
 
+Write a solution to report the product_name, year, and price for each sale_id in the Sales table.
+Return the resulting table in any order.
+The result format is in the following example.
+
+
 Table: Sales
 +-------------+-------+
 | Column Name | Type  |
