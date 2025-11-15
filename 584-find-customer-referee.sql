3a0b59e5b777e201f81e98b141c8d62c7e901865
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-15 10:05:24 -0500

    Update 584-find-customer-referee.sql

--- 584-find-customer-referee.sql
+++ 584-find-customer-referee.sql
@@ -1,39 +1,50 @@
-#584. Find Customer Referee
+/*
+584. Find Customer Referee
 
-#Table: Customer
-#+-------------+---------+
-#| Column Name | Type    |
-#+-------------+---------+
-#| id          | int     |
-#| name        | varchar |
-#| referee_id  | int     |
-#+-------------+---------+
+Find the names of the customer that are either:
+1. referred by any customer with id != 2.
+2. not referred by any customer.
+Return the result table in any order.
+The result format is in the following example.
 
-#Input: 
-#  +----+------+------------+
-#  | id | name | referee_id |
-#  +----+------+------------+
-#  | 1  | Will | null       |
-#  | 2  | Jane | null       |
-#  | 3  | Alex | 2          |
-#  | 4  | Bill | null       |
-#  | 5  | Zack | 1          |
-#  | 6  | Mark | 2          |
-#  +----+------+------------+
 
+Table: Customer
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| referee_id  | int     |
++-------------+---------+
+
+Input:
+  Customer
+  +----+------+------------+
+  | id | name | referee_id |
+  +----+------+------------+
+  | 1  | Will | null       |
+  | 2  | Jane | null       |
+  | 3  | Alex | 2          |
+  | 4  | Bill | null       |
+  | 5  | Zack | 1          |
+  | 6  | Mark | 2          |
+  +----+------+------------+
+*/
   
-#Selects the name of customers not referred by id=2 or by any customer
-  SELECT name
-    FROM Customer
-    WHERE referee_id != 2 OR referee_id IS NULL;
+
+SELECT name -- selects the customer's name
+  FROM Customer
+  WHERE referee_id != 2 OR referee_id IS NULL -- include either customers whose referee id is not 2 or customers with no referee id
 
 
-#Output: 
-#  +------+
-#  | name |
-#  +------+
-#  | Will |
-#  | Jane |
-#  | Bill |
-#  | Zack |
-#  +------+
+/*
+Output: 
+  +------+
+  | name |
+  +------+
+  | Will |
+  | Jane |
+  | Bill |
+  | Zack |
+  +------+
+*/
