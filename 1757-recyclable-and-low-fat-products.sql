f27dbf492fc3527c819d6477d7de91f2cc3ccd0e
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-14 19:23:56 -0500

    Update 1757-recyclable-and-low-fat-products.sql

--- 1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products.sql
@@ -1,36 +1,45 @@
-#1757. Recyclable and Low Fat Products
+/*
+1757. Recyclable and Low Fat Products
 
-#Table: Products
-#+-------------+---------+
-#| Column Name | Type    |
-#+-------------+---------+
-#| product_id  | int     |
-#| low_fats    | enum    |
-#| recyclable  | enum    |
-#+-------------+---------+
+Write a solution to find the ids of products that are both low fat and recyclable.
+Return the result table in any order.
+The result format is in the following example.
 
-#Input:
-#  +-------------+----------+------------+
-#  | product_id  | low_fats | recyclable |
-#  +-------------+----------+------------+
-#  | 0           | Y        | N          |
-#  | 1           | Y        | Y          |
-#  | 2           | N        | Y          |
-#  | 3           | Y        | Y          |
-#  | 4           | N        | N          |
-#  +-------------+----------+------------+
 
-    
-#Selects ID of products that are both low-fat and recyclable, then returns only these IDs
-SELECT product_id
+Table: Products
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+
+Input:
+  Products
+  +-------------+----------+------------+
+  | product_id  | low_fats | recyclable |
+  +-------------+----------+------------+
+  | 0           | Y        | N          |
+  | 1           | Y        | Y          |
+  | 2           | N        | Y          |
+  | 3           | Y        | Y          |
+  | 4           | N        | N          |
+  +-------------+----------+------------+
+*/
+
+
+SELECT product_id --selects the product id from the Products table
     FROM Products
-    WHERE low_fats = 'Y' AND recyclable = 'Y';
+    WHERE low_fats = 'Y' AND recyclable = 'Y' -- only includes products that are both low fat and recyclable
 
 
-#Output: 
-#  +-------------+
-#  | product_id  |
-#  +-------------+
-#  | 1           |
-#  | 3           |
-#  +-------------+
+/*
+Output: 
+  +-------------+
+  | product_id  |
+  +-------------+
+  | 1           |
+  | 3           |
+  +-------------+
+*/
