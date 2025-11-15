5b3b132ccee5df8757db83d93f804ae1b0642eb1
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2025-11-15 10:09:56 -0500

    Update 595-big-countries.sql

--- 595-big-countries.sql
+++ 595-big-countries.sql
@@ -1,38 +1,50 @@
-#595. Big Countries
+/*
+595. Big Countries
 
-#Table: World
-#+-------------+---------+
-#| Column Name | Type    |
-#+-------------+---------+
-#| name        | varchar |
-#| continent   | varchar |
-#| area        | int     |
-#| population  | int     |
-#| gdp         | bigint  |
-#+-------------+---------+
-
-#Input: 
-#  +-------------+-----------+---------+------------+--------------+
-#  | name        | continent | area    | population | gdp          |
-#  +-------------+-----------+---------+------------+--------------+
-#  | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
-#  | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
-#  | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
-#  | Andorra     | Europe    | 468     | 78115      | 3712000000   |
-#  | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
-#  +-------------+-----------+---------+------------+--------------+
+A country is big if:
+• it has an area of at least three million (i.e., 3000000 km2), or
+• it has a population of at least twenty-five million (i.e., 25000000).
+Write a solution to find the name, population, and area of the big countries.
+Return the result table in any order.
+The result format is in the following example.
 
 
-#Selects the name, population, and area and returns only countries with an area >= 3 million km^2 or population >= 25 million
-SELECT name, population, area
+Table: World
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| name        | varchar |
+| continent   | varchar |
+| area        | int     |
+| population  | int     |
+| gdp         | bigint  |
++-------------+---------+
+
+Input: 
+  World
+  +-------------+-----------+---------+------------+--------------+
+  | name        | continent | area    | population | gdp          |
+  +-------------+-----------+---------+------------+--------------+
+  | Afghanistan | Asia      | 652230  | 25500100   | 20343000000  |
+  | Albania     | Europe    | 28748   | 2831741    | 12960000000  |
+  | Algeria     | Africa    | 2381741 | 37100000   | 188681000000 |
+  | Andorra     | Europe    | 468     | 78115      | 3712000000   |
+  | Angola      | Africa    | 1246700 | 20609294   | 100990000000 |
+  +-------------+-----------+---------+------------+--------------+
+*/
+
+
+SELECT name, population, area -- selects the country's name, population, and area
     FROM World
-    WHERE area >= 3000000 OR population >= 25000000; 
+    WHERE area >= 3000000 OR population >= 25000000 -- includes either countries with area of at least 3 million km2 or a population of 25 million
 
 
-#Output: 
-#  +-------------+------------+---------+
-#  | name        | population | area    |
-#  +-------------+------------+---------+
-#  | Afghanistan | 25500100   | 652230  |
-#  | Algeria     | 37100000   | 2381741 |
-#  +-------------+------------+---------+
+/*
+Output: 
+  +-------------+------------+---------+
+  | name        | population | area    |
+  +-------------+------------+---------+
+  | Afghanistan | 25500100   | 652230  |
+  | Algeria     | 37100000   | 2381741 |
+  +-------------+------------+---------+
+*/
