035ee322b7c30791d413722d5520f0ccdfa72674
Alec Covey <147283675+alcove-data@users.noreply.github.com>
2026-03-03 09:39:53 -0500

    Update 619-biggest-single-number.sql

--- 619-biggest-single-number.sql
+++ 619-biggest-single-number.sql
@@ -3,6 +3,7 @@
 
 A single number is a number that appeared only once in the MyNumbers table.
 Find the largest single number. If there is no single number, report null.
+The result format is in the following example.
 
 
 Table: MyNumbers
